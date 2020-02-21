e75d6b8f969ccbaef4ebc58d241d1caf553d77d3
jodestrevin@gmail.com <jod35@gmail.com>
2020-02-20 20:02:19 -0500

    created a list view from the post model

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+class HomePageView(ListView):
+    template_name='index.html'
+    model=Post
